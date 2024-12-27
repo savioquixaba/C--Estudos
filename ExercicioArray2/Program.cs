cc5f37304b5e08009062f1fcf8c8f45a25aae739
Savio Quixaba <76010162+savioquixaba@users.noreply.github.com>
2024-12-27 18:20:13 -0300

    Add files via upload

---
+++ ExercicioArray2/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+Console.WriteLine("Quantos Alunos vão ser cadastrados? ");
+int numerodeAlunos = int.Parse(Console.ReadLine());
+
+
+//Inicializando os Arrays
+
+string[] nomes = new string[numerodeAlunos];
+double[] notas = new double[numerodeAlunos];
+
+//inicializando varial para somar as notas
+
+double somaNotas = 0;
+
+for (int i = 0; i < numerodeAlunos; i++)
+{
+    Console.WriteLine($"Digite o nome do Aluno {i + 1}: ");
+    nomes[i] = Console.ReadLine();
+
+    Console.WriteLine($"Digite a nota do Aluno {nomes[i]}: ");
+    notas[i] = double.Parse(Console.ReadLine());
+
+    somaNotas += notas[i];
+}
+for (int i = 0; i < nomes.Length; i++)
+{
+    Console.WriteLine($"Aluno: {nomes[i]}, Nota: {notas[i]}");
+}
+
+//media
+
+double media = somaNotas / nomes.Length ;
+
+Console.WriteLine($"A Média dos alunos é: {media}");
+
+Console.ReadKey();
---
+++ ExercicioArrays/Program.cs
@@ -0,0 +1,15 @@
+﻿string[] nomes = new string[5];
+
+for (int i = 0; i < nomes.Length; i++)
+{
+    Console.WriteLine($"Digite o nome do Aluno {i + 1} : ");
+    nomes[i] = Console.ReadLine();
+}
+
+Console.WriteLine("\n pulando a linha");
+foreach (var nome in nomes)
+{
+    Console.WriteLine( $"\n{nome}");
+}
+
+Console.ReadKey();
