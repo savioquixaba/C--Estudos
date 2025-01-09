c6fe94cb38e06b7765265789e0fdd5636a8b0fa2
Savio Quixaba <76010162+savioquixaba@users.noreply.github.com>
2025-01-09 20:55:09 -0300

    Exercicio Fixacao 4

---
+++ ExerciciosFixaçãoListas/ExercicioFixacao4/Pessoa.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioFixacao4
+{
+    internal class Pessoa
+    {
+        private string nome;
+        private int idade;
+        
+        public Pessoa(string nome, int idade) 
+        {
+            this.nome = nome;
+            this.idade = idade;
+        }
+        
+        
+        public string Nome
+        {
+            get{ return nome; }
+
+            set { nome = value; }
+        }
+        public int Idade
+        {
+            get { return idade; }
+
+            set { idade = value; }
+        }
+        
+        public void exibirDados()
+        {
+            Console.WriteLine($"Nome:{nome}, Idade:{idade}");
+        }
+        
+    
+    
+    
+    }
+}
