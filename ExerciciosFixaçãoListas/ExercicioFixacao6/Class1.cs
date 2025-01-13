52efaa14a2f4031bba5ed99852d0036214ab1278
Savio Quixaba <76010162+savioquixaba@users.noreply.github.com>
2025-01-13 17:59:51 -0300

    Exercicio de Fixacao 6

---
+++ ExerciciosFixaçãoListas/ExercicioFixacao6/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioFixacao6
+{
+    internal class Class1
+    {
+        public void ProcessaObjetos(params object[] objetos)
+        {
+            for (int i = 0; i < objetos.Length; i++)
+            {
+                if (objetos[i] == null)
+                {
+                    Console.WriteLine("O Objeto é null");
+                }
+                else
+                {
+                    Console.WriteLine($"O tipo do objeto é: {objetos[i].GetType()}");
+                }
+            }
+            
+        }
+    }
+}
---
+++ ExerciciosFixaçãoListas/ExercicioFixacao6/Program.cs
@@ -0,0 +1,5 @@
+﻿using ExercicioFixacao6;
+
+Class1 processa = new Class1();
+
+processa.ProcessaObjetos(null, 1, "Maria", 3.45m, new object());
